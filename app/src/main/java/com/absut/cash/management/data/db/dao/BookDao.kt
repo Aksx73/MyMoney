1fc24927efcf79e4658307185de4e963028e081e
Akshay <akshaynighot73@gmail.com>
2025-04-23 17:26:14 +0530

    show book list, implement add, edit, delete functionality

--- app/src/main/java/com/absut/cash/management/data/db/dao/BookDao.kt
+++ app/src/main/java/com/absut/cash/management/data/db/dao/BookDao.kt
@@ -3,6 +3,7 @@ package com.absut.cash.management.data.db.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
 import androidx.room.Update
@@ -12,23 +13,26 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface BookDao {
-    @Query("SELECT * FROM " + Constants.BOOKS_TABLE_NAME+ " ORDER BY created DESC")
+    @Query("SELECT * FROM books_table ORDER BY created DESC")
     fun getAllBooks(): Flow<List<Book>>
 
-    @Insert
+    @Query("SELECT * FROM books_table WHERE id=:bookId")
+    fun getBookById(bookId: Int): Flow<Book>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addBook(book: Book)
 
     @Update
     suspend fun updateBook(book: Book)
 
-    @Query("UPDATE books_table SET title=:title WHERE _id=:bookId")
+    @Query("UPDATE books_table SET title=:title WHERE id=:bookId")
     suspend fun updateBookTitle(bookId: Int, title: String)
 
     @Delete
     suspend fun deleteBook(book: Book)
 
-    @Query("DELETE FROM category_table WHERE book_id=:bookId")
-    suspend fun deleteBookCategories(bookId: Int)
+    /*@Query("DELETE FROM category_table WHERE book_id=:bookId")
+    suspend fun deleteBookCategories(bookId: Int)*/
 
     @Query("DELETE FROM entry_table WHERE book_id=:bookId")
     suspend fun deleteBookEntries(bookId: Int)
@@ -36,8 +40,8 @@ interface BookDao {
     @Transaction
     suspend fun deleteBookData(book: Book) {
         deleteBook(book)
-        deleteBookCategories(book._id)
-        deleteBookEntries(book._id)
+        //deleteBookCategories(book._id)
+        deleteBookEntries(book.id)
     }
 
 }
