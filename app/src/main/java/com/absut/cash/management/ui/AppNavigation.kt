c2281b25320994d1e95068ae25f2982286e3f03f
Akshay <akshaynighot73@gmail.com>
2025-04-29 16:29:28 +0530

    update AppNavigation to include shared viewmodel for entry list and addupdate entry screen

--- app/src/main/java/com/absut/cash/management/ui/AppNavigation.kt
+++ app/src/main/java/com/absut/cash/management/ui/AppNavigation.kt
@@ -1,35 +1,21 @@
 package com.absut.cash.management.ui
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
 import androidx.navigation.toRoute
 import com.absut.cash.management.ui.booklist.BookListScreen
 import com.absut.cash.management.ui.booklist.BookListViewModel
 import com.absut.cash.management.ui.categoryList.CategoryListViewModel
 import com.absut.cash.management.ui.categoryList.CategoryScreen
 import com.absut.cash.management.ui.entrydetail.AddUpdateEntryScreen
-import com.absut.cash.management.ui.entrydetail.EntryDetailViewModel
 import com.absut.cash.management.ui.entrydetail.EntryType
 import com.absut.cash.management.ui.entrylist.EntryListScreen
 import com.absut.cash.management.ui.entrylist.EntryListViewModel
 import kotlinx.serialization.Serializable
 
-
-/*object NavigationRoutes {
-    const val BOOK_LIST = "bookList"
-    const val ENTRY_LIST = "entryList"
-    const val ADD_UPDATE_ENTRY = "addUpdateEntry"
-    const val CATEGORY_LIST = "categoryList"
-}*/
 
 @Serializable
 data object BookListRoute
@@ -43,7 +29,7 @@ data class EntryListRoute(
 data class AddUpdateEntryRoute(
     val entryType: EntryType,
     val bookId: Int,
-    val entryId: Int? = null
+    val entryId: Int? = null,
 )
 
 @Serializable
@@ -52,6 +38,9 @@ data object CategoryListRoute
 
 @Composable
 fun AppNavigation(navController: NavHostController) {
+
+    val sharedViewModel: EntryListViewModel = hiltViewModel<EntryListViewModel>()
+
     NavHost(
         navController = navController,
         startDestination = BookListRoute,
@@ -65,7 +54,7 @@ fun AppNavigation(navController: NavHostController) {
         composable<EntryListRoute> {
             val args = it.toRoute<EntryListRoute>()
             EntryListScreen(
-                viewModel = hiltViewModel<EntryListViewModel>(),
+                viewModel = sharedViewModel,
                 navController = navController,
                 bookId = args.bookId
             )
@@ -73,11 +62,11 @@ fun AppNavigation(navController: NavHostController) {
         composable<AddUpdateEntryRoute> { backStackEntry ->
             val args = backStackEntry.toRoute<AddUpdateEntryRoute>()
             AddUpdateEntryScreen(
-                viewModel = hiltViewModel<EntryDetailViewModel>(),
+                viewModel = sharedViewModel,
                 navController = navController,
                 entryType = args.entryType,
                 bookId = args.bookId,
-                entryId = null
+                entryId = args.entryId,
             )
         }
         composable<CategoryListRoute> {
