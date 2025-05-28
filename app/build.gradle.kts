127262a0c42dacf0e662b1c30a68cf05497120cc
Akshay <akshaynighot73@gmail.com>
2025-05-28 09:41:12 +0530

    update to material 3 expressive alpha artifact version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,17 +61,17 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
     //compose
-    val composeBom = platform("androidx.compose:compose-bom:2025.02.00")
+    val composeBom = platform("androidx.compose:compose-bom:2025.05.01")
     implementation(composeBom)
     implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.4.0-alpha15")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.navigation:navigation-compose:2.8.9")
+    implementation("androidx.navigation:navigation-compose:2.9.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
     // Hilt
     implementation("com.google.dagger:hilt-android:2.56.2")
@@ -81,8 +81,8 @@ dependencies {
     ksp("androidx.room:room-compiler:2.7.1")
     implementation("androidx.room:room-ktx:2.7.1")
 
-    implementation("dev.chrisbanes.haze:haze:1.5.4")
-    implementation("dev.chrisbanes.haze:haze-materials:1.5.4")
+    implementation("dev.chrisbanes.haze:haze:1.6.2")
+    implementation("dev.chrisbanes.haze:haze-materials:1.6.2")
 
     testImplementation("androidx.room:room-testing:2.7.1")
     androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
